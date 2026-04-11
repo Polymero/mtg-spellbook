d6e28822542b5217a0e7ae3853ca99dd3f5032c3
Polymero <sebastiaanvenendaal@gmail.com>
2026-04-11 13:32:36 +0200

    202604111332

--- Spellbox/Spellbox/obj/Debug/net8.0/Spellbox.AssemblyInfo.cs
+++ Spellbox/Spellbox/obj/Debug/net8.0/Spellbox.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spellbox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee097a516fd8b90bb57b978455b28a3d6d3c28ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e9bd4e303166436243b3013588040a65659906c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spellbox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spellbox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
